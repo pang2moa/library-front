0cd2a0cab11eef81a82d81eced40bd03bb3b9e15
pangmoa <vkdahdk@gmail.com>
2024-07-30 23:01:08 +0900

    waefwaef

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,24 @@
+import { env } from 'process';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     compress: false,
     async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: '/api/:path*',
-        },
-      ];
+      if (env.NODE_ENV === 'production') {
+        return [
+          {
+            source: '/api/Member/:path*',
+            destination: 'http://222.108.42.200:8083/api/Member/:path*',
+          },
+        ];
+      } else {
+        return [
+          {
+            source: '/api/:path*',
+            destination: '/api/:path*',
+          },
+        ];
+      }
     },
     webpack(webpackConfig) {
       return {
